19ce88781b5b7771abaf7fe5d3425092576eb06c
owirsching <olivia.wirsching@gmail.com>
2023-09-12 21:05:44 -0400

    Update README and paintstroke sizing

--- M3.py
+++ M3.py
@@ -11,9 +11,9 @@ PALETTE = {
     "purple": "#B377AB",
     "light_purple": "#D3B2BB",
     "dusty_pink": "#EDD7D3",
-    "green": "#1D7D76",
+    "green": "#729C77",
     "taupe": "#F0DABD",
-    "orange": "#D27E20",
+    "orange": "#DC8825",
     "yellow": "#EEB34D",
     "dark_green": "#22636F",
     "light_green": "#C1C292",
@@ -84,24 +84,25 @@ def main():
         "medium":  {"small": 0.6, "medium": 0.3, "large": 0.1},
         "large":  {"small": 0.3, "medium": 0.6, "large": 0.1}
     },object_matrix={
-        "tree": {"tree": 0.2, "sky": 0.3, "orange_bush": 0.1, "green_bush": 0.05, "ground": 0.05, "ocean": 0.3},
-        "sky": {"tree": 0.05, "sky": 0.3, "orange_bush": 0.3, "green_bush": 0.2, "ground": 0.05, "ocean": 0.1},
-        "orange_bush": {"tree": 0.05, "sky": 0.05, "orange_bush": 0.2, "green_bush": 0.3, "ground": 0.3, "ocean": 0.1},
-        "green_bush": {"tree": 0.05, "sky": 0.05, "orange_bush": 0.3, "green_bush": 0.1, "ground": 0.3, "ocean": 0.2},
-        "ground": {"tree": 0.3, "sky": 0.05, "orange_bush": 0.3, "green_bush": 0.2, "ground": 0.05, "ocean": 0.1},
-        "ocean": {"tree": 0.1, "sky": 0.3, "orange_bush": 0.05, "green_bush": 0.2, "ground": 0.05, "ocean": 0.3}        
+        "tree": {"tree": 0.2, "sky": 0.2, "orange_bush": 0.1, "green_bush": 0.05, "ground": 0.05, "ocean": 0.3, "yellow_bush": 0.1},
+        "sky": {"tree": 0.05, "sky": 0.3, "orange_bush": 0.05, "green_bush": 0.1, "ground": 0.3, "ocean": 0.1, "yellow_bush": 0.1},
+        "orange_bush": {"tree": 0.05, "sky": 0.3, "orange_bush": 0.1, "green_bush": 0.05, "ground": 0.2, "ocean": 0.1, "yellow_bush": 0.2},
+        "green_bush": {"tree": 0.05, "sky": 0.3, "orange_bush": 0.05, "green_bush": 0.05, "ground": 0.3, "ocean": 0.2, "yellow_bush": 0.05},
+        "yellow_bush": {"tree": 0.05, "sky": 0.2, "orange_bush": 0.05, "green_bush": 0.1, "ground": 0.3, "ocean": 0.2, "yellow_bush": 0.1},
+        "ground": {"tree": 0.3, "sky": 0.2, "orange_bush": 0.1, "green_bush": 0.05, "ground": 0.05, "ocean": 0.1, "yellow_bush": 0.2},
+        "ocean": {"tree": 0.05, "sky": 0.3, "orange_bush": 0.05, "green_bush": 0.2, "ground": 0.05, "ocean": 0.3, "yellow_bush": 0.05}        
     },object_color_matrix={
     "tree": {
-        "green": {"green": 0.5, "yellow": 0.2, "orange": 0.1, "light_green": 0.2},
-        "yellow": {"green": 0.1, "yellow": 0.2, "orange": 0.5, "light_green": 0.2},
-        "orange": {"green": 0.5, "yellow": 0.2, "orange": 0.2, "light_green": 0.1},
-        "light_green": {"green": 0.5, "yellow": 0.1, "orange": 0.2, "light_green": 0.2},
+        "green": {"green": 0.2, "yellow": 0.2, "teal": 0.1, "light_green": 0.5},
+        "yellow": {"green": 0.1, "yellow": 0.2, "teal": 0.5, "light_green": 0.2},
+        "teal": {"green": 0.5, "yellow": 0.2, "teal": 0.2, "light_green": 0.1},
+        "light_green": {"green": 0.5, "yellow": 0.1, "teal": 0.2, "light_green": 0.2},
         },
     "sky": {
         "light_blue": {"light_blue": 0.5, "medium_blue": 0.2, "dusty_pink": 0.1, "light_purple": 0.2},
         "medium_blue": {"light_blue": 0.1, "medium_blue": 0.2, "dusty_pink": 0.5, "light_purple": 0.2},
         "dusty_pink": {"light_blue": 0.5, "medium_blue": 0.2, "dusty_pink": 0.2, "light_purple": 0.1},
-        "light_purple": {"light_blue": 0.5, "medium_blue": 0.1, "dusty_pink": 0.2, "light_purple": 0.2},
+        "light_purple": {"light_blue": 0.5, "medium_blue": 0.1, "dusty_pink": 0.2, "light_purple": 0.2}
         },
     "orange_bush": {
         "orange": {"orange": 0.5, "yellow": 0.2, "purple": 0.1, "dark_orange": 0.2},
@@ -110,10 +111,16 @@ def main():
         "dark_orange": {"orange": 0.5, "yellow": 0.1, "purple": 0.2, "dark_orange": 0.2},
         },
     "green_bush": {
-        "green": {"green": 0.2, "yellow": 0.1, "dark_blue": 0.5, "dark_green": 0.2},
-        "yellow": {"green": 0.1, "yellow": 0.2, "dark_blue": 0.2, "dark_green": 0.5},
-        "dark_blue": {"green": 0.5, "yellow": 0.2, "dark_blue": 0.2, "dark_green": 0.1},
-        "dark_green": {"green": 0.2, "yellow": 0.1, "dark_blue": 0.5, "dark_green": 0.2},
+        "green": {"green": 0.2, "dark_green": 0.1, "teal": 0.5, "light_green": 0.2},
+        "dark_green": {"green": 0.1, "dark_green": 0.2, "teal": 0.2, "light_green": 0.5},
+        "teal": {"green": 0.5, "dark_green": 0.2, "teal": 0.2, "light_green": 0.1},
+        "light_green": {"green": 0.2, "dark_green": 0.1, "teal": 0.5, "light_green": 0.2},
+        },
+    "yellow_bush": {
+        "green": {"green": 0.2, "yellow": 0.1, "orange": 0.5, "light_green": 0.2},
+        "yellow": {"green": 0.1, "yellow": 0.2, "orange": 0.2, "light_green": 0.5},
+        "orange": {"green": 0.5, "yellow": 0.2, "orange": 0.2, "light_green": 0.1},
+        "light_green": {"green": 0.2, "yellow": 0.1, "orange": 0.5, "light_green": 0.2},
         },
     "ground": {
         "dark_blue": {"dark_blue": 0.5, "purple": 0.2, "medium_blue": 0.1, "dark_green": 0.2},
@@ -122,10 +129,10 @@ def main():
         "dark_green": {"dark_blue": 0.5, "purple": 0.1, "medium_blue": 0.2, "dark_green": 0.2},
         },
     "ocean": {
-        "teal": {"teal": 0.5, "light_blue": 0.2, "medium_blue": 0.1, "taupe": 0.2},
-        "light_blue": {"teal": 0.1, "light_blue": 0.2, "medium_blue": 0.5, "taupe": 0.2},
-        "medium_blue": {"teal": 0.5, "light_blue": 0.2, "medium_blue": 0.2, "taupe": 0.1},
-        "taupe": {"teal": 0.5, "light_blue": 0.1, "medium_blue": 0.2, "taupe": 0.2},
+        "teal": {"teal": 0.5, "light_blue": 0.2, "medium_blue": 0.1, "dark_green": 0.2},
+        "light_blue": {"teal": 0.1, "light_blue": 0.2, "medium_blue": 0.5, "dark_green": 0.2},
+        "medium_blue": {"teal": 0.5, "light_blue": 0.2, "medium_blue": 0.2, "dark_green": 0.1},
+        "dark_green": {"teal": 0.5, "light_blue": 0.1, "medium_blue": 0.2, "dark_green": 0.2},
         }
 })
     
@@ -135,7 +142,7 @@ def main():
     current_object="sky"
 
     # For 100 Flowers
-    for _ in range (0,100):
+    for _ in range (0,500):
         # Pick a paintbrush size
         size = painter.get_next_size(current_size)
         current_size = size
@@ -144,14 +151,26 @@ def main():
         object = painter.get_next_object(current_object)
         current_object = object
         current_color = list(painter.object_color_matrix[current_object].keys())[0]
-        print(current_object)
 
         # Choose a random position on the canvas
-        pos = (randint(-150,150), randint(-150,150))   
+        if current_object == "sky": 
+            pos = (randint(-100,150), randint(50,150)) 
+        elif current_object == "ocean": 
+            pos = (randint(-150,150), randint(-50,50)) 
+        elif current_object == "tree": 
+            pos = (randint(-150,-50), randint(-50,150)) 
+        elif current_object == "ground": 
+            pos = (randint(-150,150), randint(-150,-50))
+        elif current_object == "orange_bush": 
+            pos = (randint(-150,150), randint(-100,-50))  
+        elif current_object == "green_bush": 
+            pos = (randint(-150,150), randint(-150,-50))  
+        elif current_object == "yellow_bush": 
+            pos = (randint(-150,0), randint(-150,0))  
         direction = 0
         
         # For 9 paintstrokes
-        for _ in range(0, 9):
+        for _ in range(0, 10):
             # Initialize the turtle 
             turtle = Turtle()
             turtle.penup()
@@ -159,7 +178,7 @@ def main():
             turtle.setpos(pos)
             direction += 40
             turtle.left(direction)
-            turtle.fd(10)
+            turtle.fd(8)
 
             # Choose a new paint color
             color = painter.get_next_color(current_color, current_object)
@@ -168,11 +187,11 @@ def main():
 
             # Change the paintbrush size to what was selected before
             if size == "small":
-                turtle.shapesize(0.5,0.5)
+                turtle.shapesize(0.5,0.25)
             elif size == "medium":
-                turtle.shapesize(1,1)
+                turtle.shapesize(0.5,0.25)
             else: 
-                turtle.shapesize(1.5, 1)
+                turtle.shapesize(0.5, 0.25)
             
             # Paint the circles 
             turtle.pendown()
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# cc-m3-pointillism
-Computational Creativity Assignment M3. Generating a pointillist painting using Markov Chains to decide the color and size of points on the canvas. Inspired by Henri Edmond Cross' Two Women by the Shore, Mediterranean.
+### Two Markov Chains by the Shore, Mediterranean 
+Computational Creativity Assignment M3. 
+
+This project generates a pointillist painting inspired by Henri Edmond Cross' "Two Women by the Shore, Mediterranean" using Markov Chains. This piece is an example of pointillism, where each brush stroke looks like a disticnt point on the canvas. This project uses Turtle library on a 300 x 300 pixel "canvas" to paint a piece that resembles Edmond Cross' original by using colors taken from the artwork and implementing pointilism by only printing disticnt strokes on the screen.  
+
+**Two Markov Chains by the Shore, Mediterranean** paints 500 circles with 10 brush strokes composing each circle. Each of these circles represents an object within Edmond Cross' painting (sky, ocean, tree, green bush, yellow bush, orange bush, or ground). Each object has a particular color profile. For example, the sky can be colored with light blue, medium blue, dusty pink, or light purple paints. The location of an object is restricted to its general location in the original painting (e.g. any sky circle can only be placed toward the top of the screen's canvas). These restrictions are hard coded for each object. However, within each available area, the location of a circle object is randomly chosen. 
+
+Markov Chains determine which object will be painted next and which colors in the object's color profile will be used for each paintstroke in that object. 
+
+# How to set up and run the code 
+1. Navigate to the cc-m3-pointillism folder
+2. Run 'python3 M3.py' in the terminal 
+3. A window should appear and the painting will begin 
+4. Wait a long time. The painting is ready when the black triangle stops flashing in the middle. 
+5. The window will stay open for 15 seconds after the completion of the painting before closing automatically. 
+
+# Personal Meaning 
+I first saw Two Women by the Shore, Mediterranean in 2019 when visiting The Barnes Foundation in Philadelphia. I was immediately struck by the painting. The crisp imagery paired with the vibrant colors is captivating. I am not one to usually enjoy museums or buy anything from a gift store, but I loved this painting so much that I bought a scarf with the paiting and a miniature painting as well. The miniature has traveled with me up to Brunswick and is hanging in my room. 
+
+Every museum I have been to since, I always look to see if any of Edmond Cross' works are hanging on the walls. Even when I do find one, they are not as magical as this one. So, when thinking of a source of inspiration of visual art, I was immediately drawn to the idea of using this painting in some way. I am drawn both to the colors and the pointillism. So, I decided to have both of those features be the main focus of the project. Even though, the computer version does not completely capture the beauty of the original, it captures the essence of its composition. 
